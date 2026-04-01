5f71c5339494683d01e97b6200aff51b560cbca7
swagatknaik148 <swagatnaik08@gmail.com>
2026-04-01 17:34:49 +0530

    update

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -79,10 +79,19 @@ useEffect(()=>{
               <div></div>
               <div className='flex items-center'>
                 {activeSectionIndex !== 0 && (
-                  <button className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-slate-500 hover:bg-gray-50 transition-all'>
-                    <ChevronLeft />
+                  <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.max(prevIndex - 1, 0))}
+                   className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-slate-500 hover:bg-gray-50 transition-all'
+                  disabled={activeSectionIndex === 0}>
+                    <ChevronLeft className="size-4"/> Previous
                   </button>
                 )}
+                <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.min(prevIndex + 1, sections.length - 1))}
+                   className={'flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${}'}
+                  disabled={activeSectionIndex === sections.length - 1}>
+                    <ChevronLeft className="size-4"/> Next
+                  </button>
+                   
+
               </div>
 
             </div>
